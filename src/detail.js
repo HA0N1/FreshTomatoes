e8efb3517fbc80dcd46c1d5e7981d6daf67880d9
kim yujin <yujin3009@naver.com>
2024-01-11 22:15:49 +0900

    주석 추가

--- src/detail.js
+++ src/detail.js
@@ -11,21 +11,26 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", opti
   .then((response) => response.json())
   .then((response) => {
     const results = response["results"];
-    let orgnlTitle = results[6].original_title;
-    console.log(String(results[6].title));
+
+    // 메인에서 눌러서 넘어올 수 있는 건 아직 못했어요..
+    // top rated 0번째 인덱스 영화 불러오기
+    let orgnlTitle = results[0].original_title;
+    console.log(String(results[0].title));
     console.log(String(orgnlTitle));
-    if (String(results[6].title) === String(orgnlTitle)) {
+    // 원제와 제목이 같다면 원제는 공백 처리
+    if (String(results[0].title) === String(orgnlTitle)) {
       orgnlTitle = "";
     }
+    //select-movie 클래스 안에 삽입
     let movieHtml = `
-        <img src="https://image.tmdb.org/t/p/original${results[6].poster_path}" alt="${results[6].title}" />
+        <img src="https://image.tmdb.org/t/p/original${results[0].poster_path}" alt="${results[0].title}" />
         <div class = "movie-info">
-          <p class="movie-title">${results[6].title}&nbsp;<span>${orgnlTitle}</span></p>
+          <p class="movie-title">${results[0].title}&nbsp;<span>${orgnlTitle}</span></p>
           <hr>
-          <p><span>Release_date</span> : ${results[6].release_date}</p>
-          <p class="movie-genre"><span>Genre</span> : ${results[6].genre_ids}</p>
-          <p><span>Rating</span> : ★ ${results[6].vote_average}</p>
-          <p class = "overview">${results[6].overview}</p>
+          <p><span>Release_date</span> : ${results[0].release_date}</p>
+          <p class="movie-genre"><span>Genre</span> : ${results[0].genre_ids}</p>
+          <p><span>Rating</span> : ★ ${results[0].vote_average}</p>
+          <p class = "overview">${results[0].overview}</p>
           
         </div>
       `;
