056ec9a769e9ffdaf61fdce64993d29b33f525eb
kim yujin <yujin3009@naver.com>
2024-01-11 16:40:57 +0900

    상세정보 너비 조정

--- src/detail.js
+++ src/detail.js
@@ -10,7 +10,29 @@ const options = {
 fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", options)
   .then((response) => response.json())
   .then((response) => {
-    let results = response["results"];
-    console.log(results);
+    const results = response["results"];
+    let movieHtml = `
+        <img src="https://image.tmdb.org/t/p/original${results[0].poster_path}" alt="${results[0].title}" />
+        <div class = "movie-info">
+          <p class="movie-title">${results[0].title}&nbsp;<span>${results[0].original_title}</span></p>
+          <p>Release_date : ${results[0].release_date} | Genre : ${results[0].genre_ids}</p>
+          <p>Rating: ${results[0].vote_average}</p>
+          <p>${results[0].overview}</p>
+          
+        </div>
+      `;
+    document.getElementById("select-movie").innerHTML += movieHtml;
+
+    // results.forEach((a) => {
+    //   let movieHtml = `
+    //   <div class="movie-info" id="${a.id}">
+    //     <img src="https://image.tmdb.org/t/p/original${a.poster_path}" alt="${a.title}">
+    //     <h2 class="card-title">${a.title}</h2>
+    //     <p>${a.overview}</p>
+    //     <p>Rating: ${a.vote_average}</p>
+    //   </div>
+    //   `;
+
+    // });
   })
   .catch((err) => console.error(err));
