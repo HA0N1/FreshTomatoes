fb2d6dd4d4de90b071f3add99dc173ce13e2a4ee
HA0N1 <intor48@gmail.com>
2024-01-15 02:13:32 +0900

    데이터 불러오기 기능구현. 좋아요 버튼 기능 구현 css 추가 작업 필요

--- src/testpush.js
+++ src/testpush.js
@@ -3,6 +3,10 @@ let rating = document.getElementById("rating");
 let reviewContent = document.getElementById("reviewContent");
 let password = document.getElementById("password");
 let form = document.getElementById("save");
+let cardHeader = document.querySelector(".cardHeader");
+let spanName = document.querySelector(".spanName");
+let cardContainer = document.querySelector(".cardContainer");
+let likeCount = document.querySelector(".likeCount");
 
 class Board {
   constructor(name, rating, reviewContent, password) {
@@ -57,3 +61,38 @@ form.addEventListener("submit", (e) => {
     console.error(e);
   }
 });
+
+let boardsObj = JSON.parse(localStorage.getItem("boards"));
+// 저장된 데이터들을 순회하며 각 변수에 저장 후, 추가하기
+let boardsValue = function () {
+  for (i = 0; i < boardsObj.length; i++) {
+    let boardsName = boardsObj[i]._name;
+    let boardsRating = boardsObj[i]._rating;
+    let boardsReviewContent = boardsObj[i]._reviewContent;
+    let boardsPassword = boardsObj[i]._password; // 언젠간 쓰겠지 뭐
+    let starRating = "⭐".repeat(boardsRating);
+    //카드 추가
+    let review = `<div class="card">
+  <div class="cardHeader">
+    <span class="spanName">${boardsName}</span>
+    <span class="rating">${starRating}</span>
+    <hr />
+    <p>${boardsReviewContent}</p>
+    <button class="likeButton">좋아요👍</button>
+    <span class="likeCount">0</span>
+  </div>
+</div>`;
+    cardContainer.innerHTML += review;
+    // console.log(boardsName, boardsRating, boardsReviewContent, boardsPassword);
+  }
+};
+boardsValue();
+
+let likeCountFunc = function () {
+  let conut = 0;
+  let sum = 0;
+  for (i = 0; i < 10; i++) {
+    sum += i;
+  }
+  return sum;
+};
